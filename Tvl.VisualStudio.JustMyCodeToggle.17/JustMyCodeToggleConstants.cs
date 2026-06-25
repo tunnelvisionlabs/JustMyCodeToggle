365df8d07c52b5496f05c8f01560a37e55cfa078
Sam Harwell <sam@tunnelvisionlabs.com>
2026-06-24 21:15:21 -0500

    refactor: share command constants
    Move the shared command-set GUID and command ID constants into a linked
    shared source file. Keep only the package GUID constants in each package
    project so the legacy and VS2022+ packages can retain distinct identities.
    
    Restore the explanatory comments in the VS2022+ project file to match the
    legacy project configuration.

--- Tvl.VisualStudio.JustMyCodeToggle.17/JustMyCodeToggleConstants.cs
+++ Tvl.VisualStudio.JustMyCodeToggle.17/JustMyCodeToggleConstants.cs
@@ -3,15 +3,8 @@
 
 namespace Tvl.VisualStudio.JustMyCodeToggle
 {
-    using Guid = System.Guid;
-
-    internal static class JustMyCodeToggleConstants
+    internal static partial class JustMyCodeToggleConstants
     {
         public const string GuidJustMyCodeTogglePackageString = "79B6220F-7F5A-4ECE-B5C4-2CA3D7D5FA36";
-
-        public const string GuidJustMyCodeToggleCommandSetString = "B1317E30-CFDA-47CA-90A0-95E894B150A0";
-        public static readonly Guid GuidJustMyCodeToggleCommandSet = new Guid("{" + GuidJustMyCodeToggleCommandSetString + "}");
-
-        public static readonly int CmdidJustMyCodeToggle = 0x0100;
     }
 }
--- Tvl.VisualStudio.JustMyCodeToggle/JustMyCodeToggleConstants.cs
+++ Tvl.VisualStudio.JustMyCodeToggle/JustMyCodeToggleConstants.cs
@@ -3,15 +3,8 @@
 
 namespace Tvl.VisualStudio.JustMyCodeToggle
 {
-    using Guid = System.Guid;
-
-    internal static class JustMyCodeToggleConstants
+    internal static partial class JustMyCodeToggleConstants
     {
         public const string GuidJustMyCodeTogglePackageString = "73702199-D0C5-4863-8203-99D41B34DD2D";
-
-        public const string GuidJustMyCodeToggleCommandSetString = "B1317E30-CFDA-47CA-90A0-95E894B150A0";
-        public static readonly Guid GuidJustMyCodeToggleCommandSet = new Guid("{" + GuidJustMyCodeToggleCommandSetString + "}");
-
-        public static readonly int CmdidJustMyCodeToggle = 0x0100;
     }
 }
